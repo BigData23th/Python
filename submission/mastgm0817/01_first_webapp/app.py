32273eea6473df12a353f6ba674b8f0e8695a558
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 00:47:41 +0900

    과제1

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -8,9 +8,13 @@ st.write(
     ### 노마드 코더
     > 이유: 기본적으로 영어로 설명을 해줘서
 
+    * [노마드코더](https://www.youtube.com/@nomadcoders)
+
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ### 드림코딩
     > 이유: 프론트 웹개발을 공부 할 때 가장 도움이 되었던 유튜브채널
+
+    * [드림코딩](https://www.youtube.com/@dream-coding)
 
     # 참고 사이트
     * [icons8](https://icons8.com/)
