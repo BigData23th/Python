e9978155e10659c1038858a5801e4a9d6172b9ee
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 01:20:38 +0900

    과제1

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -5,30 +5,32 @@ import streamlit as st
 st.write(
     """
     ## 나에게 가장 도움이 될 것 같은 유튜브
-    ### 노마드 코더
-    > 이유: 최신 IT트렌드와 이슈 등을 영어와 설명을 해주고 한글 자막까지 제공해줘서
+    > ### 노마드 코더
+
+    + [노마드코더](https://www.youtube.com/@nomadcoders)
 
     ```
-    [노마드코더](https://www.youtube.com/@nomadcoders)
+    이유: 최신 IT트렌드와 이슈 등을 영어와 설명을 해주고 한글 자막까지 제공해줘서
     ```
 
     ---
 
     ## 다른 학생들에게 도움이 될 것 같은 유튜브
-    ### 드림코딩
-    > 이유: <u>프론트 웹개발을 기초 공부</u> 할 때 가장 도움이 되었던 유튜브채널  
-    <u>프론트 웹개발을 기초 공부</u> 할 때 가장 도움이 되었던 유튜브채널
+    > ### 드림코딩
+
+    + [드림코딩](https://www.youtube.com/@dream-coding)
 
     ```
-    [드림코딩](https://www.youtube.com/@dream-coding)
+    이유: 프론트 웹개발을 기초 공부 할 때 가장 도움이 되었던 유튜브채널  
     ```
+    
 
     ---
 
     ##### 참고 사이트
-    * [icons8](https://icons8.com/)
-    * [flaticon](https://www.flaticon.com/)
-    * [pixabay](https://pixabay.com/ko/)
+    - [icons8](https://icons8.com/)
+    - [flaticon](https://www.flaticon.com/)
+    - [pixabay](https://pixabay.com/ko/)
 
     """
 )
