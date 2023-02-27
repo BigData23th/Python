79e23900515932c2f102f7fa030012fa35bb4095
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 01:55:59 +0900

    과제1

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -8,23 +8,23 @@ st.write(
     """
 
     ## 나에게 가장 도움이 될 것 같은 유튜브
-    > ### _노마드 코더_
+    > ### _노마드 코더_ :smile:
 
     [![이미지](https://i.ytimg.com/vi/x_Yw2f161CU/hqdefault.jpg)](https://www.youtube.com/@nomadcoders)
 
     ```
-    이유: 최신 IT트렌드와 이슈 등을 영어와 설명을 해주고 한글 자막까지 제공해줘서 :smile:
+    이유: 최신 IT트렌드와 이슈 등을 영어와 설명을 해주고 한글 자막까지 제공해줘서
     ```
 
     ---
 
     ## 다른 학생들에게 도움이 될 것 같은 유튜브
-    > ### _드림코딩_
+    > ### _드림코딩_ :+1:
 
     [![이미지](https://i.ytimg.com/vi/TTLHd3IyErM/hqdefault.jpg)](https://www.youtube.com/@dream-coding)
 
     ```
-    이유: 프론트 웹 개발을 기초 공부 할 때 가장 도움이 되었던 유튜브채널 :+1:
+    이유: 프론트 웹 개발을 기초 공부 할 때 가장 도움이 되었던 유튜브채널
     ```
     
 
