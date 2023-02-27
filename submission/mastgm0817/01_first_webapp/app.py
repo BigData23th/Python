8d3065798b478cc3df6bf0c2e815b573bdeb8fb0
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 01:45:17 +0900

    과제1

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -6,9 +6,7 @@ import streamlit as st
 st.write(
 
     """
-
-    <img src="https://theorydb.github.io/assets/img/think/2019-06-25-think-future-ai-1.png" width="300" height="200"> 
-
+    ![이미지](https://cdn.pixabay.com/photo/2023/01/28/06/57/man-7750139_960_720.png){: width="100%" height="100%"}
 
     ## 나에게 가장 도움이 될 것 같은 유튜브
     > ### _노마드 코더_
