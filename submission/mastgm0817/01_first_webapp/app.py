ccf087767ab9daad469e1190306953b0d446305d
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 01:32:57 +0900

    과제1

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -1,13 +1,15 @@
 # streamlit 라이브러리 호출
 import streamlit as st
 
+st.image(
+    <img src="https://theorydb.github.io/assets/img/think/2019-06-25-think-future-ai-1.png" width="300" height="200"> 
+)
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """
     ## 나에게 가장 도움이 될 것 같은 유튜브
     > ### _노마드 코더_
 
-    + [노마드코더](https://www.youtube.com/@nomadcoders)
     [![이미지](https://i.ytimg.com/vi/x_Yw2f161CU/hqdefault.jpg)](https://www.youtube.com/@nomadcoders)
 
     ```
@@ -19,10 +21,10 @@ st.write(
     ## 다른 학생들에게 도움이 될 것 같은 유튜브
     > ### _드림코딩_
 
-    + [드림코딩](https://www.youtube.com/@dream-coding)
+    [![이미지](https://i.ytimg.com/vi/TTLHd3IyErM/hqdefault.jpg)](https://www.youtube.com/@dream-coding)
 
     ```
-    이유: 프론트 웹개발을 기초 공부 할 때 가장 도움이 되었던 유튜브채널  
+    이유: 프론트 웹 개발을 기초 공부 할 때 가장 도움이 되었던 유튜브채널  
     ```
     
 
@@ -32,11 +34,8 @@ st.write(
     - [icons8](https://icons8.com/)
     - [flaticon](https://www.flaticon.com/)
     - [pixabay](https://pixabay.com/ko/)
+    - [MarshallK](https://marshallku.com/)
 
     """
 )
 
-# https://pixabay.com/ko
-st.image(
-    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
-)
